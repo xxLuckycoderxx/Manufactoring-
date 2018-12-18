90240ecb32ca507a6a5fc88ea415765e9e32b941
Student675 <moses.tibambuya.2022@student.lhschools.org>
2018-12-18 08:16:56 -0500

    half way done

--- src/buissness.java
+++ src/buissness.java
@@ -6,16 +6,54 @@ public class buissness {
 
   Scanner keyboard=new Scanner(System.in);
 
- System.out.println("Number of materials");
- int supplies=keyboard.nextInt();
+ System.out.println("Amount of lemons");
+ double lemons=keyboard.nextInt();
+
+ System.out.println("Cost 0f lemons");
+double costOne=keyboard.nextDouble();
+
+System.out.println("Amount of Sugar ");
+double sweet=keyboard.nextDouble();
+
+ System.out.println("Cost of Sugar ");
+ double costTwo=keyboard.nextDouble();
+
+ System.out.println("Amount of cups ");
+double cupsnumb=-keyboard.nextDouble();
+
+System.out.println("Cost  of Cups");
+double cupsco=keyboard.nextDouble();
 
 
- System.out.println("Cost of materials");
- int cost=keyboard.nextInt();
+double Lemonz=(lemons/costOne);
+
+double Cupz=(cupsnumb/cupsco);
+
+double biterr=(cupsnumb/costTwo);
+
+
+
+double totalCom=(Lemonz+Cupz+biterr);
+System.out.println(totalCom);
+
+
+
+
+
+
+
+/*
+
+
+ System.out.println("Total cost");
+ double totalCost=keyboard.nextDouble();
+
  System.out.println("Sales ");
- int sales=keyboard.nextInt();
+ double sales=keyboard.nextDouble();
 
- System.out.println("Your profit is $" +(  sales-cost));
+ System.out.println("Your profit is $" +(  sales-totalCost));
+*/
     }
 }
+
 
