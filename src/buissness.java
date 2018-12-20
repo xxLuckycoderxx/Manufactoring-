de8bf5fcd5b7925bfa24088a519ca19a444e6180
xxLuckycoderxx <42833597+xxLuckycoderxx@users.noreply.github.com>
2018-12-19 22:41:14 -0500

    Update buissness.java
    final change made

--- src/buissness.java
+++ src/buissness.java
@@ -23,8 +23,13 @@ double cupsnumb=-keyboard.nextDouble();
 
 System.out.println("Cost  of Cups");
 double cupsco=keyboard.nextDouble();
+        
+System.out.println("how many cups sold");
+double scam=keyboard.nextDouble();
 
-
+System.out.println("what rate");
+double rate =keyboardd.nextDouble();
+        
 double Lemonz=(lemons/costOne);
 
 double Cupz=(cupsnumb/cupsco);
@@ -36,23 +41,23 @@ double biterr=(cupsnumb/costTwo);
 double totalCom=(Lemonz+Cupz+biterr);
 System.out.println(totalCom);
 
+double profit=(scam*rate);
+System.out.println(rate);
+        
+System.out.println("Any days?");
+int days=keyboard.nextInt();
+        
+if (days!= 0 ){
+   double newTotal=(left*day);
+    
+System.out.println("This is your new total"(newTotal));
 
 
 
+double left=profit-totalCom);
 
+ System.out.println("Your profit is $" +(left));
 
-
-/*
-
-
- System.out.println("Total cost");
- double totalCost=keyboard.nextDouble();
-
- System.out.println("Sales ");
- double sales=keyboard.nextDouble();
-
- System.out.println("Your profit is $" +(  sales-totalCost));
-*/
     }
 }
 
